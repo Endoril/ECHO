b81cbc54dbc6ef94c4b6ea7ebd7136392d0cc828
Endoril <140304617+Endoril@users.noreply.github.com>
2024-10-01 22:14:00 +0200

    Einnahme Hinzufügen Abfrage hinzugefügt. Gültigkeit der Eingabe wird geprüft.

--- Form2.Designer.vb
+++ Form2.Designer.vb
@@ -22,14 +22,14 @@ Partial Class FormEinnahmeHinzufügen
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        TextBox1 = New TextBox()
+        TxtBetrag = New TextBox()
         DateTimePicker1 = New DateTimePicker()
         RadioButton1 = New RadioButton()
         RadioButton2 = New RadioButton()
         ComboBox1 = New ComboBox()
         TextBox2 = New TextBox()
-        btnSpeichern = New Button()
-        btnAbbrechen = New Button()
+        BtnSpeichern = New Button()
+        BtnAbbrechen = New Button()
         RadioButton3 = New RadioButton()
         RadioButton4 = New RadioButton()
         Label1 = New Label()
@@ -37,17 +37,25 @@ Partial Class FormEinnahmeHinzufügen
         GroupBox1 = New GroupBox()
         GroupBox2 = New GroupBox()
         Label3 = New Label()
+        Label4 = New Label()
+        GroupBox3 = New GroupBox()
+        RadioButton5 = New RadioButton()
+        RadioButton6 = New RadioButton()
+        RadioButton7 = New RadioButton()
+        RadioButton8 = New RadioButton()
         GroupBox1.SuspendLayout()
         GroupBox2.SuspendLayout()
+        GroupBox3.SuspendLayout()
         SuspendLayout()
         ' 
-        ' TextBox1
+        ' TxtBetrag
         ' 
-        TextBox1.Location = New Point(306, 99)
-        TextBox1.Multiline = True
-        TextBox1.Name = "TextBox1"
-        TextBox1.Size = New Size(174, 34)
-        TextBox1.TabIndex = 0
+        TxtBetrag.Font = New Font("Segoe UI", 12F)
+        TxtBetrag.Location = New Point(306, 99)
+        TxtBetrag.Multiline = True
+        TxtBetrag.Name = "TxtBetrag"
+        TxtBetrag.Size = New Size(174, 34)
+        TxtBetrag.TabIndex = 0
         ' 
         ' DateTimePicker1
         ' 
@@ -95,25 +103,25 @@ Partial Class FormEinnahmeHinzufügen
         TextBox2.Size = New Size(284, 27)
         TextBox2.TabIndex = 5
         ' 
-        ' btnSpeichern
+        ' BtnSpeichern
         ' 
-        btnSpeichern.Font = New Font("Segoe UI", 12F)
-        btnSpeichern.Location = New Point(602, 415)
-        btnSpeichern.Name = "btnSpeichern"
-        btnSpeichern.Size = New Size(173, 64)
-        btnSpeichern.TabIndex = 6
-        btnSpeichern.Text = "Speichern"
-        btnSpeichern.UseVisualStyleBackColor = True
+        BtnSpeichern.Font = New Font("Segoe UI", 12F)
+        BtnSpeichern.Location = New Point(602, 415)
+        BtnSpeichern.Name = "BtnSpeichern"
+        BtnSpeichern.Size = New Size(173, 64)
+        BtnSpeichern.TabIndex = 6
+        BtnSpeichern.Text = "Speichern"
+        BtnSpeichern.UseVisualStyleBackColor = True
         ' 
-        ' btnAbbrechen
+        ' BtnAbbrechen
         ' 
-        btnAbbrechen.Font = New Font("Segoe UI", 12F)
-        btnAbbrechen.Location = New Point(48, 415)
-        btnAbbrechen.Name = "btnAbbrechen"
-        btnAbbrechen.Size = New Size(173, 59)
-        btnAbbrechen.TabIndex = 7
-        btnAbbrechen.Text = "Abbrechen"
-        btnAbbrechen.UseVisualStyleBackColor = True
+        BtnAbbrechen.Font = New Font("Segoe UI", 12F)
+        BtnAbbrechen.Location = New Point(48, 415)
+        BtnAbbrechen.Name = "BtnAbbrechen"
+        BtnAbbrechen.Size = New Size(173, 59)
+        BtnAbbrechen.TabIndex = 7
+        BtnAbbrechen.Text = "Abbrechen"
+        BtnAbbrechen.UseVisualStyleBackColor = True
         ' 
         ' RadioButton3
         ' 
@@ -187,40 +195,110 @@ Partial Class FormEinnahmeHinzufügen
         Label3.TabIndex = 14
         Label3.Text = "Betrag"
         ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(491, 228)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(149, 20)
+        Label4.TabIndex = 15
+        Label4.Text = "Datum der Einnahme"
+        ' 
+        ' GroupBox3
+        ' 
+        GroupBox3.Controls.Add(RadioButton8)
+        GroupBox3.Controls.Add(RadioButton7)
+        GroupBox3.Controls.Add(RadioButton6)
+        GroupBox3.Controls.Add(RadioButton5)
+        GroupBox3.Location = New Point(491, 266)
+        GroupBox3.Name = "GroupBox3"
+        GroupBox3.Size = New Size(284, 113)
+        GroupBox3.TabIndex = 16
+        GroupBox3.TabStop = False
+        ' 
+        ' RadioButton5
+        ' 
+        RadioButton5.AutoSize = True
+        RadioButton5.Location = New Point(14, 26)
+        RadioButton5.Name = "RadioButton5"
+        RadioButton5.Size = New Size(96, 24)
+        RadioButton5.TabIndex = 0
+        RadioButton5.TabStop = True
+        RadioButton5.Text = "monatlich"
+        RadioButton5.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton6
+        ' 
+        RadioButton6.AutoSize = True
+        RadioButton6.Location = New Point(14, 73)
+        RadioButton6.Name = "RadioButton6"
+        RadioButton6.Size = New Size(78, 24)
+        RadioButton6.TabIndex = 1
+        RadioButton6.TabStop = True
+        RadioButton6.Text = "jährlich"
+        RadioButton6.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton7
+        ' 
+        RadioButton7.AutoSize = True
+        RadioButton7.Location = New Point(157, 26)
+        RadioButton7.Name = "RadioButton7"
+        RadioButton7.Size = New Size(75, 24)
+        RadioButton7.TabIndex = 2
+        RadioButton7.TabStop = True
+        RadioButton7.Text = "täglich"
+        RadioButton7.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton8
+        ' 
+        RadioButton8.AutoSize = True
+        RadioButton8.Location = New Point(157, 73)
+        RadioButton8.Name = "RadioButton8"
+        RadioButton8.Size = New Size(109, 24)
+        RadioButton8.TabIndex = 17
+        RadioButton8.TabStop = True
+        RadioButton8.Text = "wöchentlich"
+        RadioButton8.UseVisualStyleBackColor = True
+        ' 
         ' FormEinnahmeHinzufügen
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.Control
         ClientSize = New Size(818, 532)
+        Controls.Add(GroupBox3)
+        Controls.Add(Label4)
         Controls.Add(Label3)
         Controls.Add(GroupBox2)
         Controls.Add(GroupBox1)
         Controls.Add(Label2)
         Controls.Add(Label1)
-        Controls.Add(btnAbbrechen)
-        Controls.Add(btnSpeichern)
+        Controls.Add(BtnAbbrechen)
+        Controls.Add(BtnSpeichern)
         Controls.Add(TextBox2)
         Controls.Add(ComboBox1)
         Controls.Add(DateTimePicker1)
-        Controls.Add(TextBox1)
+        Controls.Add(TxtBetrag)
         Name = "FormEinnahmeHinzufügen"
         Text = "Form2"
         GroupBox1.ResumeLayout(False)
         GroupBox1.PerformLayout()
         GroupBox2.ResumeLayout(False)
         GroupBox2.PerformLayout()
+        GroupBox3.ResumeLayout(False)
+        GroupBox3.PerformLayout()
         ResumeLayout(False)
         PerformLayout()
     End Sub
 
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TxtBetrag As TextBox
     Friend WithEvents DateTimePicker1 As DateTimePicker
     Friend WithEvents RadioButton1 As RadioButton
     Friend WithEvents RadioButton2 As RadioButton
     Friend WithEvents ComboBox1 As ComboBox
     Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents btnSpeichern As Button
-    Friend WithEvents btnAbbrechen As Button
+    Friend WithEvents BtnSpeichern As Button
+    Friend WithEvents BtnAbbrechen As Button
     Friend WithEvents RadioButton3 As RadioButton
     Friend WithEvents RadioButton4 As RadioButton
     Friend WithEvents Label1 As Label
@@ -228,4 +306,10 @@ Partial Class FormEinnahmeHinzufügen
     Friend WithEvents GroupBox1 As GroupBox
     Friend WithEvents GroupBox2 As GroupBox
     Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents RadioButton6 As RadioButton
+    Friend WithEvents RadioButton5 As RadioButton
+    Friend WithEvents RadioButton8 As RadioButton
+    Friend WithEvents RadioButton7 As RadioButton
 End Class
--- Form2.vb
+++ Form2.vb
@@ -1,18 +1,34 @@
 ﻿Public Class FormEinnahmeHinzufügen
 
     ' Klick-Event für den Speichern-Button im modalen Fenster
-    Private Sub btnSpeichern_Click(sender As Object, e As EventArgs) Handles btnSpeichern.Click
-        ' Hier kannst du den Code hinzufügen, um die Einnahme zu speichern
-        ' Z. B. Daten aus den Textboxen lesen und verarbeiten
+    Private Sub BtnSpeichern_Click(sender As Object, e As EventArgs) Handles BtnSpeichern.Click
+        Dim betrag As Decimal
 
-        ' Schließe das modale Fenster nach dem Speichern
-        Me.Close()
+        ' Versuchen, den Text in TxtBetrag in eine Dezimalzahl zu konvertieren
+        If Decimal.TryParse(TxtBetrag.Text, betrag) Then
+            ' Überprüfen, ob es maximal 2 Nachkommastellen gibt
+            Dim splitBetrag As String() = TxtBetrag.Text.Split(","c)
+
+            ' Überprüfen, ob der Vorkomma-Teil maximal 7 Ziffern enthält
+            If splitBetrag(0).Length <= 7 AndAlso (splitBetrag.Length = 1 OrElse splitBetrag(1).Length = 2) Then
+                ' Betrag ist gültig, jetzt kannst du ihn speichern
+                MessageBox.Show("Eingabe ist gültig! Betrag: " & betrag.ToString())
+                ' Hier kommt der Code zum Speichern der Daten hin
+            Else
+                ' Fehler: Zu viele Vorkommastellen oder falsche Anzahl Nachkommastellen
+                MessageBox.Show("Bitte gib einen Betrag mit maximal 7 Vorkommastellen und genau 2 Nachkommastellen ein.")
+            End If
+        Else
+            ' Fehler: Keine gültige Zahl
+            MessageBox.Show("Bitte gib einen gültigen Betrag ein.")
+        End If
     End Sub
 
     ' Klick-Event für den Abbrechen-Button im modalen Fenster
-    Private Sub btnAbbrechen_Click(sender As Object, e As EventArgs) Handles btnAbbrechen.Click
+    Private Sub BtnAbbrechen_Click(sender As Object, e As EventArgs) Handles BtnAbbrechen.Click
         ' Schließe das modale Fenster, ohne etwas zu speichern
         Me.Close()
     End Sub
 
 End Class
+
